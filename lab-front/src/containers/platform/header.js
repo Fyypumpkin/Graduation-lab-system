573f5f0d2a251a43476d8324446d2c3618ba8719
Fyypumpkin <fyypumpkin@gmail.com>
2018-05-03 15:42:26 +0800

    feature: 处理菜单和框架头部信息

--- lab-front/src/containers/platform/header.js
+++ lab-front/src/containers/platform/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Layout, Menu, Dropdown, Icon, Input } from 'antd'
 import '../../App.css'
-import image from '../../logo.svg'
+import image from '../../images/hdu.png'
 import '../../themes/header.css'
 
 const {Header} = Layout
@@ -15,14 +15,20 @@ const menu = (
     <Menu.Item key="3">
       <Icon type="logout" style={{marginRight: '10px', marginLeft: '10px'}}/>
       <a onClick={() => {
-        logout('user')
       }} style={{display: 'inline-block'}}>注销</a>
+    </Menu.Item>
+    <Menu.Item key="4">
+      <Icon type="user" style={{marginRight: '10px', marginLeft: '10px'}}/>
+      <a onClick={() => {
+      }} style={{display: 'inline-block'}}>个人信息</a>
     </Menu.Item>
   </Menu>
 )
 
 function logout (name) {
-  alert('logout')
+  localStorage.clear()
+  window.location.reload()
+  // TODO 服务器登出
 }
 
 class OpHeader extends React.Component {
@@ -46,8 +52,8 @@ class OpHeader extends React.Component {
             <Dropdown overlay={menu}>
               <div className="tail-logo">
                 <span style={{fontSize: '16px'}}>
-                  <Icon type="user"/>
-                  &nbsp;{this.props.username}
+                  欢迎 &nbsp;{this.props.username}
+                    <Icon type="setting" style={{marginLeft: '10px', cursor: 'pointer'}}/>
                 </span>
               </div>
             </Dropdown>
