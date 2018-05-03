871b1a77d6a9a3fcd1293d54ae1995fa434a4c59
Fyypumpkin <fyypumpkin@gmail.com>
2018-05-03 15:43:31 +0800

    feature：增加全局store，增加人员类型标识

---
+++ lab-front/src/stores/store/common/global-store.js
@@ -0,0 +1,23 @@
+/**
+ * @author fyypumpkin on 2018/5/3.
+ */
+import {observable, action} from 'mobx'
+
+class GlobalStore {
+  /**
+   * SA CP CA 超级管理员，普通人员，普通管理员
+   * @type {string}
+   */
+  @observable roleType = ''
+
+  @action.bound
+  setRoleType (type) {
+    this.roleType = type
+  }
+
+  get getRoleType () {
+    return this.roleType
+  }
+}
+
+export default new GlobalStore()
